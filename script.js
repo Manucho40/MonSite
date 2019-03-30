26750439279f85a3b24d6b47ff62b42c9d5bbb1c
Manucho40 <rickypaine40@gmail.com>
2019-03-30 23:53:46 +0000

    avantfin

---
+++ script.js
@@ -0,0 +1,88 @@
+//jquery
+
+/*alert("hello");
+
+for(i=0; i<=5; i++)
+{
+alert(i);
+
+}
+ /*(document).ready(function(){
+
+ 	$(".nike").animate();
+ 	*$("#click").hover(function(){
+
+ 		$(".demo").html("La clase de L2")
+
+
+ 	})
+   $("#demo").html("la Classe de l2");
+    
+ });*/
+
+
+
+//son des  variables primitives
+
+
+//objet
+/*let classe={
+   eleve1: "yede",
+   eleve2: "limard"
+};
+
+console.log(classe);*/
+//fonction
+/*function maClasse(){
+	console.log("Bonjour");
+}
+
+maClasse();*/
+
+
+
+/*let newDate = new Date();
+document.body.innerHTML = "<h1> La date est " + newDate + "</h1>";*/
+/*let classe = prompt("Entrer votre chiffre");
+if(classe==3){
+	alert("correct");
+}
+else{
+	alert("perdu");
+}*/
+
+
+//console.log(newDtae);
+/*let x;
+x = "lymard";
+
+let x = "Lymard";
+
+let x = 60;
+
+// un objet
+let x ={
+
+	name: "lymard";
+	age: 46;
+	genre: "male";
+}
+
+//concatenation
+let nom= "AKA";
+let prenom = "stephanie";
+
+let id = nom + " " + prenom;
+//les variables
+let x = 3; // int
+let x = 1.2 //float
+let x = -76; //Negative integer
+ 
+ //Array
+ let x = [34, "yeyete", true];
+ 
+
+   //connaitre le nombre de colonne
+ classe.length
+  //integration de tag dans html a partir de java script
+ */
